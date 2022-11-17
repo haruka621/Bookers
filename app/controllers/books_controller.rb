fb99b2448b11a769d4ab56be0131f26a7b1f0a35
EC2 Default User <115263610+haruka621@users.noreply.github.com>
2022-11-17 19:42:33 +0000

    [finish] bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,25 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def index
+    @books = Book.all.order(id: :asc)
+    @book = Book.new
+  end
+
   def create
+    @books = Book.all.order(id: :asc)
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      render :index
     end
   end
 
-  def index
-    @books = Book.all.order(created_at: :desc)
-    @book = Book.new
-  end
+
+
 
   def show
     @book = Book.find(params[:id])
@@ -24,21 +29,24 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[ :notice] = "Book was successfully created"
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    flash[:notice] = "Book was successfully created"
+    redirect_to book_path(@book.id)
+   else
+     render :edit
+   end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[ :notice] = "Book was successfully created"
     redirect_to '/books'
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
